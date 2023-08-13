4c54c272b716bca51c82a9fcb1308765cff2d33a
JamesBerlioz <mavlut18@gmail.com>
2023-08-13 22:06:07 +0300

    finished main workflow

--- client/src/components/modals/CreateBrand.js
+++ client/src/components/modals/CreateBrand.js
@@ -1,9 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
 import Button from "react-bootstrap/Button";
 import Modal from "react-bootstrap/Modal";
 import { Form } from "react-bootstrap";
+import { createBrand } from "../../http/deviceApi";
 
 const CreateBrand = ({ show, onHide }) => {
+  const [value, setValue] = useState("");
+
+  const addBrand = () => {
+    createBrand({ name: value }).then((data) => {
+      setValue("");
+      onHide();
+    });
+  };
+
   return (
     <Modal show={show} onHide={onHide} size="lg" centered>
       <Modal.Header closeButton>
@@ -13,14 +23,18 @@ const CreateBrand = ({ show, onHide }) => {
       </Modal.Header>
       <Modal.Body>
         <Form>
-          <Form.Control placeholder={"Введите название бренда"} />
+          <Form.Control
+            value={value}
+            onChange={(e) => setValue(e.target.value)}
+            placeholder={"Введите название бренда"}
+          />
         </Form>
       </Modal.Body>
       <Modal.Footer>
         <Button variant="outline-danger" onClick={onHide}>
           Закрыть
         </Button>
-        <Button variant="outline-success" onClick={onHide}>
+        <Button variant="outline-success" onClick={addBrand}>
           Добавить
         </Button>
       </Modal.Footer>
--- client/src/pages/Shop.js
+++ client/src/pages/Shop.js
@@ -8,6 +8,7 @@ import DeviceList from "../components/DeviceList";
 import { observer } from "mobx-react-lite";
 import { Context } from "..";
 import { fetchBrands, fetchDevices, fetchTypes } from "../http/deviceApi";
+import Pages from "../components/Pages";
 
 const Shop = observer(() => {
   const { device } = useContext(Context);
@@ -15,8 +16,23 @@ const Shop = observer(() => {
   useEffect(() => {
     fetchTypes().then((data) => device.setTypes(data));
     fetchBrands().then((data) => device.setBrands(data));
-    fetchDevices().then((data) => device.setDevices(data.rows));
+    fetchDevices(null, null, 1, 3).then((data) => {
+      device.setDevices(data.rows);
+      device.setTotalCount(data.count);
+    });
   }, []);
+
+  useEffect(() => {
+    fetchDevices(
+      device.selectedType.id,
+      device.selectedBrand.id,
+      device.page,
+      3
+    ).then((data) => {
+      device.setDevices(data.rows);
+      device.setTotalCount(data.count);
+    });
+  }, [device.page, device.selectedType, device.selectedBrand]);
 
   return (
     <Container>
@@ -27,6 +43,7 @@ const Shop = observer(() => {
         <Col md={9}>
           <BrandBar />
           <DeviceList />
+          <Pages />
         </Col>
       </Row>
     </Container>
